881778ed3cbffcb67519c6514e048aa91082043d
Jason Stroup <jasonstroup@Stroup-MacBook-Pro.local>
2017-08-22 10:25:04 -0600

    JS written, stop light working

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-(function() {
-  'use strict';
-
   // YOUR CODE HERE
+(function() {
+ $(document).ready(function(){
+    'use strict';
+
+ $('#stopButton').click(function(){
+   console.log("this works");
+$('#stopLight').toggleClass("stop");
+});
+
+ })();
+ $('#slowButton').click(function(){
+  console.log("this works too");
+   $('#slowLight').toggleClass('slow');
+});
+
+$('#goButton').click(function(){
+  console.log("this works 3");
+  $('#goLight').toggleClass('go');
+});
+
 })();
